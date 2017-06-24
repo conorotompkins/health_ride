af6ecbf06f2fed96bc307b6fc2931cb34c0d9f1c
conorotompkins <conorotompkins@gmail.com>
2017-06-24 12:44:03 -0400

    moved data loading code to its own script

--- exploratory analysis.R
+++ exploratory analysis.R
@@ -5,41 +5,6 @@ theme_set(theme_bw())
 
 rm(list = ls())
 
-data_list <- list.files(pattern = ".csv")
-data_list <- lapply(data_list, read_csv)
-data <- bind_rows(data_list)
-
-colnames(data) <- tolower(colnames(data))
-colnames(data) <- gsub(" ", "_", colnames(data))
-
-data_long <- data %>% 
-  mutate(start_date_time = starttime,
-         stop_date_time = stoptime) %>%
-  select(-c(starttime, stoptime)) %>% 
-  gather(date_time_type, date_time, c(start_date_time, stop_date_time)) %>% 
-  select(date_time_type, date_time, everything()) %>% 
-  mutate(date_time_2 = date_time) %>% 
-  separate(date_time, " ", into = c("date", "time")) %>% 
-  mutate(date = mdy(date),
-         time = hm(time),
-         hour = hour(time),
-         wday = wday(date, label = TRUE),
-         is_weekday = ifelse(wday %in% c("Mon", "Tues", "Wed", "Thurs", "Fri"), "weekday", "weekend")) %>% 
-  gather(location_id_type, location_id, c(from_station_id, to_station_id)) %>% 
-  gather(location_name_type, location_name, c(from_station_name, to_station_name)) %>% 
-  select(date_time_type, 
-         is_weekday, 
-         date, 
-         time, 
-         hour,
-         wday, 
-         date_time_2, 
-         location_id_type, 
-         location_id, 
-         location_name_type,
-         location_name,
-         everything())
-
 data_long
 
 table(data_long$is_weekday, data_long$wday)
@@ -63,9 +28,22 @@ data_long %>%
 data_long %>%
   ggplot(aes(hour, color = wday)) +
   geom_freqpoly(stat = "density") +
-  facet_wrap(~wday, 
-             ncol = 1)
+  scale_x_continuous(breaks = c(0:23))
 
+data_long %>%
+  ggplot(aes(hour, color = wday)) +
+  geom_freqpoly() +
+  scale_x_continuous(breaks = c(0:23))
+
+data_long %>%
+  ggplot(aes(hour, color = is_weekday)) +
+  geom_freqpoly(stat = "density") +
+  scale_x_continuous(breaks = c(0:23))
+
+data_long %>%
+  ggplot(aes(hour, color = is_weekday)) +
+  geom_freqpoly() +
+  scale_x_continuous(breaks = c(0:23))
 
 
 
@@ -76,3 +54,46 @@ data_long %>%
   mutate(cum_sum = cumsum(n)) %>% 
   ggplot(aes(date, cum_sum)) +
   geom_line()
+
+data_long %>%
+  select(year, yday) %>% 
+  mutate(year = as.factor(year)) %>% 
+  group_by(year, yday) %>% 
+  count() %>% 
+  ungroup() %>% 
+  group_by(year) %>% 
+  mutate(cum_sum = cumsum(n)) %>% 
+  ggplot(aes(yday, cum_sum,
+             color = year,
+             group = year)) +
+  geom_line()
+
+data_long %>%
+  select(month, mday) %>% 
+  #mutate(year = as.factor(year)) %>% 
+  group_by(month, mday) %>% 
+  count() %>% 
+  ungroup() %>% 
+  group_by(month) %>% 
+  mutate(cum_sum = cumsum(n)) %>% 
+  ggplot(aes(mday, cum_sum,
+             color = month,
+             group = month)) +
+  geom_line()
+
+data_long %>%
+  select(year, month, mday) %>% 
+  mutate(year = as.factor(year)) %>% 
+  group_by(year, month, mday) %>% 
+  count() %>% 
+  ungroup() %>% 
+  group_by(year, month) %>% 
+  mutate(cum_sum = cumsum(n)) %>% 
+  ggplot(aes(mday, cum_sum,
+             color = month)) +
+  geom_line() +
+  facet_wrap(~year,
+             ncol = 1)
+
+
+?lubridate
---
+++ load_data.R
@@ -0,0 +1,52 @@
+library(tidyverse)
+library(lubridate)
+
+theme_set(theme_bw())
+
+rm(list = ls())
+
+data_list <- list.files(pattern = ".csv")
+data_list <- lapply(data_list, read_csv)
+data <- bind_rows(data_list)
+
+colnames(data) <- tolower(colnames(data))
+colnames(data) <- gsub(" ", "_", colnames(data))
+
+data_long <- data %>% 
+  mutate(start_date_time = starttime,
+         stop_date_time = stoptime) %>%
+  select(-c(starttime, stoptime)) %>% 
+  gather(date_time_type, date_time, c(start_date_time, stop_date_time)) %>% 
+  select(date_time_type, date_time, everything()) %>% 
+  mutate(date_time_2 = date_time) %>% 
+  separate(date_time, " ", into = c("date", "time")) %>% 
+  mutate(date = mdy(date),
+         year = year(date),
+         month = month(date, label = TRUE),
+         time = hm(time),
+         hour = hour(time),
+         wday = wday(date, label = TRUE),
+         is_weekday = ifelse(wday %in% c("Mon", "Tues", "Wed", "Thurs", "Fri"), "weekday", "weekend"),
+         yday = yday(date),
+         mday = mday(date)) %>% 
+  gather(location_id_type, location_id, c(from_station_id, to_station_id)) %>% 
+  gather(location_name_type, location_name, c(from_station_name, to_station_name)) %>% 
+  select(date_time_type, 
+         is_weekday, 
+         date, 
+         year,
+         month,
+         time, 
+         hour,
+         wday,
+         yday,
+         mday,
+         date_time_2, 
+         location_id_type, 
+         location_id, 
+         location_name_type,
+         location_name,
+         everything())
+
+data_wide <- data_long %>% 
+  spread(location_name_type, location_name)

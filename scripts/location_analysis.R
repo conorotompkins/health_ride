e9e1660dfec03ee20381b45f05477ca13715d985
Conor Tompkins <conorotompkins@gmail.com>
2017-09-10 03:59:01 +0000

    troubleshooting spread()

--- scripts/location_analysis.R
+++ scripts/location_analysis.R
@@ -9,14 +9,32 @@ source("scripts/load_stations_data.R")
 
 rm(list = c("data", "data_list"))
 
-df_locations <- data_long %>% 
+data_long %>% 
   select(location_name_type, location_name) %>% 
+  count(location_name_type, location_name, sort = TRUE)
+
+df_locations <- data_long %>% 
+  select(location_name_type, location_name) %>%
+  group_by(row_number, location_name_type, location_name) %>% 
+  mutate(row_number = row_number()) %>%
+  spread(location_name_type, location_name)
+
+
+filter(!is.na(to_station_name), !is.na(from_station_name))
+
+group_by(location_name_type, location_name) %>% 
+  summarise(number_of_trips = n()) %>% 
+  arrange(desc(number_of_trips)) %>% 
+  
+  
+  
+  
+
+df_locations <- df_locations %>% 
   left_join(locations, by = c("location_name" = "station_name")) %>% 
-  select(location_name_type, location_name, latitude, longitude) %>% 
-  group_by(location_name_type, location_name, latitude, longitude) %>% 
-  summarise(number_of_trips = n())
+  select(location_name_type, location_name, latitude, longitude)
 
 rm(data_long)
   
 df_locations <- df_locations %>% 
-  spread(location_name_type, location_name)
+  

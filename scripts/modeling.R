cf1a8dd97bee29ba5fbc1226dd63f05dbdf7442d
Conor Tompkins <conorotompkins@gmail.com>
2017-10-28 15:56:57 +0000

    iterated model

--- scripts/modeling.R
+++ scripts/modeling.R
@@ -2,18 +2,17 @@ library(tidyverse)
 library(modelr)
 library(broom)
 
-options(scipen = 9)
+options(scipen = 99)
 
 source("scripts/load_data_long.R")
 source("scripts/load_data_wide.R")
 rm(list = c("data", "data_long"))
 
+###Daily models
 df_holidays <- read_csv("data/holidays.csv") %>% 
   mutate(date = mdy(date))
 df_holidays
 
-
-###Daily models
 df_daily <- data_wide %>% 
   mutate(month = factor(month,  ordered = FALSE),
          wday = factor(wday, ordered = FALSE)) %>% 
@@ -78,7 +77,9 @@ df_daily_pred %>%
 df_daily_resid %>% 
   ggplot(aes(residual, color = model)) +
   geom_freqpoly(bins = 50)
-
+?rm
+rm(list = c("df_daily", "df_daily_pred", "df_daily_resid"))
+rm(list = c("model1", "model2", "model3", "model4", "model5"))
 
 ###Daily Station Models
 
@@ -87,38 +88,37 @@ df_daily_station <- data_wide %>%
          wday = factor(wday, ordered = FALSE)) %>% 
   #filter(station_name_type == "from_station_name") %>% 
   #filter(!is.na(usertype)) %>% 
-  group_by(date, year, yday, month, wday, from_station_name, to_station_name) %>% 
-  summarize(number_of_rides = n()) %>% 
-  left_join(df_holidays) %>% 
-  replace_na(replace = list(holiday = "none"))
+  mutate(destination_type = if_else(from_station_name == to_station_name, "same_station", "different_station")) %>% 
+  group_by(date, wday, from_station_name, to_station_name, destination_type) %>% 
+  summarize(number_of_rides = n())
 df_daily_station
-class(df_daily_station$month)
-class(df_daily_station$wday)
-unique(df_daily_station$holiday)
+#class(df_daily_station$month)
+#class(df_daily_station$wday)
+#unique(df_daily_station$holiday)
 
 #sum(is.na(df$usertype))
 model1 <- lm(number_of_rides ~ from_station_name + from_station_name, data = df_daily_station)
-#model2 <- lm(number_of_rides ~ year + month + wday, data = df_daily_from)
-#model3 <- lm(number_of_rides ~ year + month * wday, data = df_daily_from)
-#model4 <- lm(number_of_rides ~ year + month * wday + holiday, data = df_daily_from)
-#model5 <- lm(number_of_rides ~ year + month * wday + holiday + from_station_name, data = df_daily_from)
+model2 <- lm(number_of_rides ~ from_station_name + destination_type, data = df_daily_station)
+#model3 <- lm(number_of_rides ~ wday + from_station_name + to_station_name, data = df_daily_station)
+#model4 <- lm(number_of_rides ~ wday + from_station_name * to_station_name, data = df_daily_station)
+#model5 <- lm(number_of_rides ~ year + month * wday + holiday + from_station_name, data = df_daily_station)
 
 df_daily_station_pred <- df_daily_station %>% 
-  gather_predictions(model1) %>% 
+  gather_predictions(model1, model2) %>% 
   rename(predicted = pred)
 
 df_daily_station_resid <- df_daily_station %>%
-  gather_residuals(model1) %>% 
+  gather_residuals(model1, model2) %>% 
   rename(residual = resid)
 
 glance(model1)
-#glance(model2)
+glance(model2)
 #glance(model3)
 #glance(model4)
 #glance(model5)
 
 tidy(model1)
-#tidy(model2)
+tidy(model2)
 #tidy(model3)
 #tidy(model4)
 #tidy(model5)
@@ -132,7 +132,6 @@ df_daily_station_resid %>%
 df_daily_station_resid %>%
   ggplot(aes(number_of_rides, residual, color = model)) +
   geom_point(alpha = .1) +
-  geom_smooth(method = "lm") +
   facet_wrap(~model,
              nrow = 1)
 
@@ -146,4 +145,6 @@ df_daily_station_resid %>%
   ggplot(aes(residual, color = model)) +
   geom_freqpoly(bins = 50)
 
+
+rm(list = c("model1", "model2"))
 #get weather data using this package http://ram-n.github.io/weatherData/

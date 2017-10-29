d77c44abe7632a203fd16cd5ab522334f5a1f707
Conor Tompkins <conorotompkins@gmail.com>
2017-10-29 17:47:53 -0400

    worked on weather data

--- scripts/weather_data_download.R
+++ scripts/weather_data_download.R
@@ -1,12 +1,28 @@
-install.packages("weatherData")
-
-library(weatherData)
 library(tidyverse)
+library(lubridate)
+library(stringr)
+library(rwunderground)
+rwunderground::set_api_key("52adb4b045871a47")
 
-#install.packages("devtools")
-#library("devtools")
-#install_github("Ram-N/weatherData")
+history(set_location(zip_code = "15222"), date = 20170701)
 
-getWeatherForDate("PIT", "2014-05-05")
 
-checkDataAvailabilityForDateRange("SFO", "2010-10-29", "2013-01-12")
+data_weather_2015 <- history_range(set_location(zip_code = "15222"), date_start = "20150531", date_end = "20151231", limit = 10)
+write_csv(data_weather_2015, "data/weather/data_weather_2015")
+
+data_wather_2016 <- history_range(set_location(zip_code = "15222"), date_start = "20160101", date_end = "20161231", limit = 10)
+data_weather_2016 <- data_wather_2016
+write_csv(data_weather_2016, "data/weather/data_weather_2016")
+
+data_weather_2017 <- history_range(set_location(zip_code = "15222"), date_start = "20170101", date_end = "20170131", limit = 10)
+write_csv("data/weather/data_weather_2017")
+
+df_weather <- bind_rows(data_weather_2015, data_weather_2016)
+
+
+df_weather_2015 <- as_tibble(df_weather_2015)
+df_weather_2015 <- df_weather_2015 %>% 
+  mutate(date = str_sub(date, 1, 8))
+
+df_weather_2015
+?stringr::str_sub()

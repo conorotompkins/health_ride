f49c3c7908e746d3b40758a4dedbf46418fdd110
Conor Tompkins <conorotompkins@gmail.com>
2017-11-02 03:01:30 +0000

    worked on script for loading weather data, modeling with weather

--- scripts/load_weather_data.R
+++ scripts/load_weather_data.R
@@ -1,4 +1,7 @@
 #load weather data
+library(tidyverse)
+library(lubridate)
+library(stringr)
 
 data_weather <- list.files(path = "data/weather/", pattern = "weather")
 data_weather_dfs <- lapply(paste0("data/weather/", data_weather), read_csv)
@@ -8,4 +11,27 @@ for(i in seq_along(data_weather_dfs)) {
   data_weather_dfs[[i]]$heat_index <- as.numeric(data_weather_dfs[[i]]$heat_index)
 }
 
-df_weather <- bind_rows(data_weather_dfs)
+df_weather <- bind_rows(data_weather_dfs) %>% 
+  mutate(date = ymd(str_sub(date, 1, 10)),
+         year = year(date),
+         month = month(date, label = TRUE)) %>% 
+  select(date, year, month, everything())
+
+#df_weather %>% 
+#  select(date, temp, rain, precip) %>% 
+#  mutate(date = ymd(str_sub(date, 1, 10))) %>% 
+#  summary()
+
+#df_weather %>% 
+#  mutate(date = ymd(str_sub(date, 1, 10))) %>% 
+#  count(date, sort = TRUE)
+
+df_weather <- df_weather %>% 
+  select(date, temp, rain, precip) %>% 
+  group_by(date) %>%
+  summarize(temp_mean = mean(temp, na.rm = TRUE),
+            temp_hi = max(temp, na.rm = TRUE),
+            temp_lo = min(temp, na.rm = TRUE),
+            precip = sum(precip, na.rm = TRUE))
+summary(df_weather)
+

aa5efd226fd8c1309591e123608fc35ec3974d37
Conor Tompkins <conorotompkins@gmail.com>
2017-11-04 19:33:31 -0400

    fixed mismatched station names

--- scripts/load_stations_data.R
+++ scripts/load_stations_data.R
@@ -9,3 +9,10 @@ for (i in seq_along(data_list)) {
 
 data_station_locations <- bind_rows(data_list) %>% 
   distinct()
+
+data_station_locations$station_name[data_station_locations$station_name == "North Shore Trail & Ft Duquesne Bridge"] <- "North Shore Trail & Fort Duquesne Bridge"
+data_station_locations$station_name[data_station_locations$station_name == "Fort Duquesne Blvd & 7th"] <- "Fort Duquesne Blvd & 7th St"
+data_station_locations$station_name[data_station_locations$station_name == "Atwood St & Bates"] <- "Atwood St & Bates St"
+data_station_locations$station_name[data_station_locations$station_name == "Alder St & S Higland Ave"] <- "Alder St & S Highland Ave"
+data_station_locations$station_name[data_station_locations$station_name == "42nd St & Penn Ave"] <- "42nd & Penn Ave."
+data_station_locations$station_name[data_station_locations$station_name == "Liberty Ave & S Millvale (West Penn Hospital)"] <- "Liberty Ave & S Millvale Ave (West Penn Hospital)"

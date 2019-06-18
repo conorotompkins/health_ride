523c5a9fa87de0975d60ae6cf869c4674c4a79e9
Conor <conorotompkins@gmail.com>
2019-06-17 22:25:53 -0400

    added code to manually plot forecast, cv diagnostics

--- scripts/prophet_prediction.R
+++ scripts/prophet_prediction.R
@@ -26,17 +26,36 @@ forecast
 plot(m, forecast)
 prophet_plot_components(m, forecast)
 
-forecast %>% 
-  left_join(df)
+df_aug <- forecast %>% 
+  mutate(ds = ymd(ds)) %>% 
+  left_join(df) %>% 
+  mutate(.resid = y - yhat)
+
+df_aug %>% 
+  ggplot(aes(x = ds)) +
+    geom_ribbon(aes(ymin = yhat_lower, ymax = yhat_upper), alpha = .2, fill = "blue") +
+    geom_line(aes(y = yhat), color = "blue") +
+    geom_smooth(aes(y = yhat), color = "red") +
+    geom_point(aes(y = y))
+
+max <- 8000
+
+df_aug %>%
+  ggplot(aes(y, yhat)) +
+  geom_point() +
+  geom_smooth(method = "lm") +
+  coord_equal(xlim = c(0, max), ylim = c(0, max))
+
+df_aug %>% 
+  ggplot(aes(.resid)) +
+  geom_density()
 
 glimpse(forecast)
 glimpse(df)
 
-forecast %>% 
-  ggplot(aes(ds)) +
-  geom_line(aes(y = trend), color = "black") +
-  geom_line(aes(y = yhat), color = "red")
-
-forecast %>% 
-  ggplot(aes(trend, yhat)) +
+df_cv <- cross_validation(m, initial = 730, period = 180, horizon = 365, units = 'days')
+performance_metrics(df_cv) %>% 
+  as_tibble() %>% 
+  ggplot(aes(horizon, rmse)) +
   geom_point()
+plot_cross_validation_metric(df_cv, metric = "mape")

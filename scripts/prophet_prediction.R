466464290a1247c1cbc28c002405dc09aab7f04a
Conor Tompkins <conorotompkins@gmail.com>
2019-06-21 17:08:19 +0000

    refactored ETL, added theme_ipsum(), refractored cv code

--- scripts/prophet_prediction.R
+++ scripts/prophet_prediction.R
@@ -1,15 +1,21 @@
 library(tidyverse)
 library(lubridate)
 library(prophet)
+library(janitor)
+library(hrbrthemes)
 
-source("scripts/load_data_long.R")
+options(scipen = 999)
 
-df <- data_long %>% 
-  select(date) %>% 
-  mutate(ds = ymd(date)) %>% 
-  count(ds) %>% 
-  rename(y = n)
-df
+theme_set(theme_ipsum())
+
+source("scripts/load_data.R")
+  
+df <- df %>% 
+  count(date) %>% 
+  rename(ds = date,
+         y = n)
+  
+glimpse(df)
 
 m <- prophet(df)
 
@@ -43,8 +49,8 @@ max <- 8000
 df_aug %>%
   ggplot(aes(y, yhat)) +
   geom_point() +
-  geom_smooth(method = "lm") +
-  coord_equal(xlim = c(0, max), ylim = c(0, max))
+  geom_smooth(method = "lm") #+
+  #coord_equal(xlim = c(0, max), ylim = c(0, max))
 
 df_aug %>% 
   ggplot(aes(.resid)) +
@@ -53,9 +59,28 @@ df_aug %>%
 glimpse(forecast)
 glimpse(df)
 
-df_cv <- cross_validation(m, initial = 730, period = 180, horizon = 365, units = 'days')
+df_cv <- cross_validation(m, initial = 365*2, period = 180, horizon = 365, units = 'days')
+
+df_cv
+
 performance_metrics(df_cv) %>% 
   as_tibble() %>% 
-  ggplot(aes(horizon, rmse)) +
-  geom_point()
+  gather(metric, measure, -horizon) %>% 
+  ggplot(aes(horizon, measure)) +
+  geom_line() +
+  facet_wrap(~metric, scales = "free_y",
+             ncol = 1)
+
+unique(df_cv$cutoff)
+
+df_cv %>% 
+  ggplot(aes(ds,)) +
+  geom_line(aes(y = yhat), color = "blue") +
+  geom_ribbon(aes(ymin = yhat_lower, ymax = yhat_upper), fill = "blue", alpha = .3) +
+  geom_point(aes(y = y)) +
+  geom_vline(xintercept = as.numeric(as_datetime(unique(df_cv$cutoff))), size = 2)
+
 plot_cross_validation_metric(df_cv, metric = "mape")
+
+test <- predictive_samples(m, df) %>% 
+  as_tibble()

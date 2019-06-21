a3a3d78e288a5ece9a13899880e184ff329dc9cf
Conor Tompkins <conorotompkins@gmail.com>
2019-06-21 17:08:34 +0000

    created refactored load_data.R script

---
+++ scripts/load_data.R
@@ -0,0 +1,50 @@
+library(tidyverse)
+library(lubridate)
+library(janitor)
+
+files <- dir("data/ride_data", ".csv")
+
+df <- str_c("data/ride_data/", files) %>% 
+  set_names() %>% 
+  map_df(read_csv) %>% 
+  clean_names() %>% 
+  mutate(start_date_time = starttime,
+         stop_date_time = stoptime) %>%
+  select(-c(starttime, stoptime)) %>% 
+  gather(date_time_type, date_time, c(start_date_time, stop_date_time)) %>% 
+  select(date_time_type, date_time, everything()) %>% 
+  mutate(date_time_2 = date_time) %>% 
+  separate(date_time, " ", into = c("date", "time")) %>% 
+  mutate(id = row_number(),
+         date = mdy(date),
+         year = year(date),
+         quarter = quarter(date),
+         month = month(date, label = TRUE),
+         week = week(date),
+         time = hm(time), 
+         hour = hour(time),
+         wday = wday(date, label = TRUE),
+         is_weekday = ifelse(wday %in% c("Mon", "Tues", "Wed", "Thurs", "Fri"), "weekday", "weekend"),
+         yday = yday(date),
+         mday = mday(date)) %>% 
+  arrange(date) %>% 
+  mutate(trip_duration = (tripduration / 60) / 60) %>% 
+  gather(station_id_type, station_id, c(from_station_id, to_station_id)) %>% 
+  gather(station_name_type, station_name, c(from_station_name, to_station_name)) %>% 
+  select(date_time_type, 
+         is_weekday, 
+         date, 
+         year,
+         quarter,
+         month,
+         #time, commented out, might be causing error 
+         hour,
+         wday,
+         yday,
+         mday,
+         date_time_2, 
+         station_id_type, 
+         station_id, 
+         station_name_type,
+         station_name,
+         everything())

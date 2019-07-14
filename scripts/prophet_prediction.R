887b2e4dea32782a38fb3fce8046a92b642014cd
Conor Tompkins <conorotompkins@gmail.com>
2019-07-14 20:12:40 +0000

    added code for floor and cap, reduced horizon for cv

--- scripts/prophet_prediction.R
+++ scripts/prophet_prediction.R
@@ -2,26 +2,29 @@ library(tidyverse)
 library(lubridate)
 library(prophet)
 library(janitor)
-library(hrbrthemes)
 
 options(scipen = 999)
 
-theme_set(theme_ipsum())
+theme_set(theme_bw())
 
 source("scripts/load_data.R")
   
 df <- df %>% 
   count(date) %>% 
   rename(ds = date,
-         y = n)
+         y = n) %>% 
+  mutate(floor = 0,
+         cap = 10^4)
   
 glimpse(df)
 
-m <- prophet(df)
+m <- prophet(df, growth = 'logistic')
 
 set_periods <- 365
 
-future <- make_future_dataframe(m, periods = set_periods, freq = "day")
+future <- make_future_dataframe(m, periods = set_periods, freq = "day") %>% 
+  mutate(floor = 0,
+         cap = 10^4)
 tail(future)
 tail(df)
 
@@ -31,6 +34,10 @@ forecast
 
 plot(m, forecast)
 prophet_plot_components(m, forecast)
+
+forecast %>% 
+  ggplot(aes(yhat)) +
+  geom_freqpoly()
 
 df_aug <- forecast %>% 
   mutate(ds = ymd(ds)) %>% 
@@ -42,7 +49,9 @@ df_aug %>%
     geom_ribbon(aes(ymin = yhat_lower, ymax = yhat_upper), alpha = .2, fill = "blue") +
     geom_line(aes(y = yhat), color = "blue") +
     geom_smooth(aes(y = yhat), color = "red") +
-    geom_point(aes(y = y))
+    geom_point(aes(y = y)) +
+    geom_hline(aes(yintercept = unique(df_aug$cap)), linetype = 2) +
+    geom_hline(aes(yintercept = unique(df_aug$floor)), linetype = 2)
 
 max <- 8000
 
@@ -59,7 +68,8 @@ df_aug %>%
 glimpse(forecast)
 glimpse(df)
 
-df_cv <- cross_validation(m, initial = 365*2, period = 180, horizon = 365, units = 'days')
+#df_cv <- cross_validation(m, initial = 365, period = 180, horizon = 365, units = 'days')
+df_cv <- cross_validation(m, horizon = 30, units = 'days')
 
 df_cv
 
@@ -74,13 +84,17 @@ performance_metrics(df_cv) %>%
 unique(df_cv$cutoff)
 
 df_cv %>% 
-  ggplot(aes(ds,)) +
-  geom_line(aes(y = yhat), color = "blue") +
-  geom_ribbon(aes(ymin = yhat_lower, ymax = yhat_upper), fill = "blue", alpha = .3) +
-  geom_point(aes(y = y)) +
-  geom_vline(xintercept = as.numeric(as_datetime(unique(df_cv$cutoff))), size = 2)
+  count(ds, sort = TRUE)
+
+df_cv %>% 
+  ggplot(aes(x = ds)) +
+  geom_line(aes(y = yhat), color = "blue")
+  #geom_ribbon(aes(ymin = yhat_lower, ymax = yhat_upper), fill = "blue", alpha = .3) +
+  #geom_point(aes(y = y)) +
+  #geom_vline(xintercept = as.numeric(as_datetime(unique(df_cv$cutoff))), size = .5)
 
 plot_cross_validation_metric(df_cv, metric = "mape")
 
 test <- predictive_samples(m, df) %>% 
   as_tibble()
+test
